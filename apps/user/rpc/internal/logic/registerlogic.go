807592cf485811b3e3093f8b9490723ff8cdba51
fei39 <3180324210@qq.com>
2025-12-22 18:29:59 +0800

    feat: user-rpc用户注册逻辑

--- apps/user/rpc/internal/logic/registerlogic.go
+++ apps/user/rpc/internal/logic/registerlogic.go
@@ -1,12 +1,23 @@
 package logic
 
 import (
+	"chat/apps/user/models"
+	"chat/pkg/ctxdata"
+	"chat/pkg/encrypt"
+	"chat/pkg/wuid"
 	"context"
+	"database/sql"
+	"errors"
+	"time"
 
 	"chat/apps/user/rpc/internal/svc"
 	"chat/apps/user/rpc/user"
 
 	"github.com/zeromicro/go-zero/core/logx"
+)
+
+var (
+	ErrPhoneIsRegister = errors.New("手机号已经注册过")
 )
 
 type RegisterLogic struct {
@@ -24,7 +35,55 @@ func NewRegisterLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Register
 }
 
 func (l *RegisterLogic) Register(in *user.RegisterReq) (*user.RegisterResp, error) {
-	// todo: add your logic here and delete this line
 
-	return &user.RegisterResp{}, nil
+	userEntity, err := l.svcCtx.UsersModel.FindByPhone(l.ctx, in.Phone)
+	if err != nil && !errors.Is(err, models.ErrNotFound) {
+		return nil, err
+	}
+
+	if userEntity != nil {
+		return nil, ErrPhoneIsRegister
+	}
+
+	userEntity = &models.Users{
+		Id:       wuid.GenUid(l.svcCtx.Config.Mysql.DataSource),
+		Avatar:   in.Avatar,
+		Nickname: in.Nickname,
+		Phone:    in.Phone,
+		// 通过构造sql空对象插入数据库中
+		Sex: sql.NullInt64{
+			Int64: int64(in.Sex),
+			Valid: true, // 是否写入数据库
+		},
+	}
+
+	if len(in.Password) > 0 {
+		genPassword, err := encrypt.GenPasswordHash([]byte(in.Password))
+		if err != nil {
+			return nil, err
+		}
+		// 通过构造sql空对象插入数据库中
+		userEntity.Password = sql.NullString{
+			String: string(genPassword),
+			Valid:  true,
+		}
+	}
+
+	_, err = l.svcCtx.UsersModel.Insert(l.ctx, userEntity)
+	if err != nil {
+		return nil, err
+	}
+
+	// 生成token
+	now := time.Now().Unix()
+	token, err := ctxdata.GetJwtToken(l.svcCtx.Config.Jwt.AccessSecret, now, l.svcCtx.Config.Jwt.AccessExpire,
+		userEntity.Id)
+	if err != nil {
+		return nil, err
+	}
+
+	return &user.RegisterResp{
+		Token:  token,
+		Expire: now + l.svcCtx.Config.Jwt.AccessExpire,
+	}, nil
 }
