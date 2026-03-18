579f0e10be10a3ae5323e1ef9d3920222f450717
fei39 <3180324210@qq.com>
2026-03-18 21:26:26 +0800

    feat: 构建Kafka消费者服务

---
+++ apps/task/mq/internal/handler/listen.go
@@ -0,0 +1,26 @@
+package handler
+
+import (
+	"chat/apps/task/mq/internal/handler/msgTransfer"
+	"chat/apps/task/mq/internal/svc"
+	"github.com/zeromicro/go-queue/kq"
+	"github.com/zeromicro/go-zero/core/service"
+)
+
+type Listen struct {
+	svc *svc.ServiceContext
+}
+
+func NewListen(svc *svc.ServiceContext) *Listen {
+	return &Listen{svc: svc}
+}
+
+// 返回多个消费者
+// ✨返回的是 go-zero/core/service
+func (l *Listen) Services() []service.Service {
+	return []service.Service{
+		// todo: 此处可以加载多个消费者
+		// 发送消息异步处理
+		kq.MustNewQueue(l.svc.Config.MsgChatTransfer, msgTransfer.NewMsgChatTransfer(l.svc)),
+	}
+}
