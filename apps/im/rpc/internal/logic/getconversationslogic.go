794e37d6a35aa93c3754b42fef0b0181ec83ad31
fei39 <3180324210@qq.com>
2026-04-01 01:11:45 +0800

    feat: 获取用户会话列表

--- apps/im/rpc/internal/logic/getconversationslogic.go
+++ apps/im/rpc/internal/logic/getconversationslogic.go
@@ -1,7 +1,11 @@
 package logic
 
 import (
+	"chat/apps/im/immodels"
+	"chat/pkg/xerr"
 	"context"
+	"github.com/jinzhu/copier"
+	"github.com/pkg/errors"
 
 	"chat/apps/im/rpc/im"
 	"chat/apps/im/rpc/internal/svc"
@@ -23,9 +27,55 @@ func NewGetConversationsLogic(ctx context.Context, svcCtx *svc.ServiceContext) *
 	}
 }
 
-// 获取会话
+// GetConversations 获取会话列表，并统计未读消息数
 func (l *GetConversationsLogic) GetConversations(in *im.GetConversationsReq) (*im.GetConversationsResp, error) {
-	// todo: add your logic here and delete this line
+	// 根据用户查询用户会话列表
+	data, err := l.svcCtx.ConversationsModel.FindByUserId(l.ctx, in.UserId)
+	if err != nil {
+		if err == immodels.ErrNotFound {
+			// 当前用户本身还没有去和任何人创建会话，这不是错误
+			return &im.GetConversationsResp{}, nil
+		}
+		return nil, errors.Wrapf(xerr.NewDBErr(), "ConversationsModel.FindByUserId err %v, req %v", err, in.UserId)
+	}
+	var res im.GetConversationsResp
+	copier.Copy(&res, &data)
 
-	return &im.GetConversationsResp{}, nil
+	// 根据会话列表，查询具体的会话
+	// 提取会话id切片
+	ids := make([]string, 0, len(data.ConversationList))
+	for _, conversation := range data.ConversationList {
+		ids = append(ids, conversation.ConversationId)
+	}
+	// 根据会话id查询会话详情
+	conversations, err := l.svcCtx.ConversationModel.ListByConversationIds(l.ctx, ids)
+	if err != nil {
+		return nil, errors.Wrapf(xerr.NewDBErr(), "ConversationModel.ListByConversationIds err %v, req %v", err, ids)
+	}
+
+	//  计算是否存在未读消息
+	for _, conversation := range conversations {
+		// 判空
+		if _, ok := res.ConversationList[conversation.ConversationId]; !ok {
+			continue
+		}
+		// 用户读取的消息量
+		// 计算未读消息数的逻辑：
+		// 数据定义：ConversationList中用map记录每一个会话的用户侧数据，ConversationList[Id].Total即为某个会话用户已读的消息数
+		// 			conversation则记录每个会话的具体消息，会话新增消息时会同步更新此集合的total
+		// 计算逻辑：总数 - 已读消息数 = 未读消息数
+		total := res.ConversationList[conversation.ConversationId].Total
+		// 读取的消息量小于会话的实际消息量
+		if total < int32(conversation.Total) {
+			// 以下设置的均为响应消息，未操作数据库
+			// 有新的消息
+			res.ConversationList[conversation.ConversationId].Total = int32(conversation.Total)
+			// 有多少是未读
+			res.ConversationList[conversation.ConversationId].ToRead = int32(conversation.Total) - total
+			// 更改当前会话为显示状态
+			res.ConversationList[conversation.ConversationId].IsShow = true
+		}
+	}
+
+	return &res, nil
 }
